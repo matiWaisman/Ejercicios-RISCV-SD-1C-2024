0156b8840a9fbd26b9d5c436c0be0201b28fbaba
matiWaisman <83453393+matiWaisman@users.noreply.github.com>
2024-06-07 13:50:16 -0300

    update

--- Ejercicios/1Recursivo.s
+++ Ejercicios/1Recursivo.s
@@ -21,20 +21,20 @@ max_array:
          addi a0, a0, 4
          addi a2, a2, -1
          beq a2, t1, base_case # Si es uno vamos al caso base
-         jal ra max_array
+         jal ra, max_array
          lw t0 4(sp) # Restauro la posicion original de a0
          lw t2 0(t0) # En t2 esta el valor de lo que contenia la posicion original de a0 
          lw t3 0(sp) # Restauro la longitud original de a2
-         bge a0, t2, epilogo 
+         bge a0, t2, epilogo # Si el valor de a0 es mas grande me quedo con ese entonces voy directo al epilogo, si no me quedo con t2
              add a0, zero, t2
              j epilogo
              
     base_case:
-        lw a0, 0(a0) # Retorno en a0 lo que diga la posicion de memoria 0 de a0
+        lw a0, 0(a0) # Guardo en a0 lo que diga la posicion de memoria 0 de a0
         
     epilogo:
         lw a2, 0(sp) # Restauro el valor de a2 del llamado anterior
-        lw a0, 4(sp) # Restauro el valor de a0 del llamado anterior
+        #lw a0, 4(sp) # Restauro el valor de a0 del llamado anterior
         lw ra, 8(sp) # Restauro la direccion del stack pointer del llamado anterior
         addi sp, sp, 12 # Libero espacio desapilando los dos datos
         ret
@@ -42,6 +42,6 @@ max_array:
 
 
 .data
-# Pone contiguos en memoria estos ocho datos
-array: .word 0x00000003 0x00000001 0x00000004 0x00000001 0x00000005 0x00000009 0x00000002 0x00000006
-long: .word 0x00000008
+# Pone contiguos en memoria estos cuatro datos
+array: .word 0x00000003, 0x00000001, 0x00000009
+long: .word 0x000000003
