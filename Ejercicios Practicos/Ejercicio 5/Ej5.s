f83f97bd3f3806f335a75be83ca21f774559fd56
matiWaisman <83453393+matiWaisman@users.noreply.github.com>
2024-06-11 12:02:34 -0300

    Updated ejercicio 5 para preservar convencion registros

--- Ejercicios Practicos/Ejercicio 5/Ej5.s
+++ Ejercicios Practicos/Ejercicio 5/Ej5.s
@@ -11,25 +11,29 @@ main:
     
 fibonacci:
     prologo:
-        addi sp, sp, -12 # Apilo en el stack el Ra, el parametro actual y despues voy a apilar n-1
-        sw ra, 8(sp)
-        sw a0, 4(sp)
+        
+        addi sp, sp, -16 # Apilo en el stack el Ra, el parametro actual y despues voy a apilar n-1
+        sw ra, 12(sp)
+        sw a0, 8(sp)
+        sw s0, 4(sp)
         li t0, 2
+        
         blt a0, t0, epilogo # Si a0 es 1 o 0 el valor de retorno = el valor del parametro
         # a0 > 1
         addi a0, a0, -1
         jal ra prologo
         sw a0, 0(sp) # Guardo el valor de fibo(n-1)
-        lw a0 4(sp) # Restauro el valor original de a0 como parametro
+        lw a0 8(sp) # Restauro el valor original de a0 como parametro
         addi a0, a0, -2
         jal ra prologo
-        lw t1, 0(sp) # Cargo el resultado de n-1
-        add a0, a0, t1
+        lw s0, 0(sp) # Cargo el resultado de n-1
+        add a0, a0, s0
         
             
     epilogo:
-        lw ra, 8(sp)
-        addi sp, sp, 12
+        lw ra, 12(sp)
+        lw s0, 4(sp)
+        addi sp, sp, 16
         ret
     
 .data
